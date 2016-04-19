bffa34e3f9fe897fbebb3d2b0b1f59c60aded521
James Bradley <hintonbradley@gmail.com>
2016-04-18 22:10:16 -0700

    Added code to add a new note

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.get('/users', function(req,res) {
   })
 });
 
+app.get('/notes/new', function(req,res) {
+  res.render('new_note');
+});
+
 app.get('/notes', function(req,res) {
   db.Note.all().then(function(nts){
   	console.log("nts is: ",nts);
@@ -105,4 +109,19 @@ app.post('/users', function(req,res) {
   });
 });
 
+app.post('/notes', function(req,res) {
+  var note_title = req.body.title;
+  var desc = req.body.description;
+  var phone = req.body.phone;
+  var id = req.body.user_id
+  // var u_ph = req.params.phone;
+  // var user_id = req.params.id;
+  db.Note.create({title: note_title, description: desc, UserId: id, user_phone:phone})
+    .then(function(note) {
+  res.redirect('/notes');
+  });
+});
+
+
 app.listen(3000);
+
