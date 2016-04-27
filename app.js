598f2c5ff274ed9cd1bbab9e58ef0fab776b0681
James Bradley <hintonbradley@gmail.com>
2016-04-26 19:52:51 -0700

    created new database

--- app.js
+++ app.js
@@ -62,18 +62,6 @@ app.use("/", function (req, res, next) {
 });
 
 //GET REQUESTS//
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
-
 app.get('/', function(req,res) {
   res.render('index');
 });
