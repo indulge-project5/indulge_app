f080ffd586b800845aa741ff878693a00d3162c3
James Bradley <hintonbradley@gmail.com>
2016-04-27 18:42:37 -0700

    updated twilio

--- js/twilio_api.js
+++ js/twilio_api.js
@@ -8,11 +8,11 @@ var client = require('twilio')(process.env.twilio_sid, process.env.twilio_auth_t
 // Old Twilio code:
   // var client = new twilio.RestClient(tw[0], tw[1]),
   msg = "Hello potential new user! Please visit http://afternoon-sierra-48284.herokuapp.com/confirm/" + user.phone + "/" + user.partner_phone + "/" + user.first_name + "/" + user.last_name;
-  phone = user.partner_phone;
+  phone = "=1" + user.partner_phone;
   name = user.first_name + " " + user.last_name;
   console.log("Entries are: ", user.phone, user.first_name);
   client.sms.messages.create({
-    to: "+1" + phone,
+    to: phone,
     from: process.env.twilio_phone,
     body: msg
 }, function(error, message) {
